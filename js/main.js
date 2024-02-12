e99e619500db6b9912423ed074c8a55f936a4fe8
Hisui02 <alvarolgleiva@gmail.com>
2024-02-12 13:40:18 +0000

    added main cards working on carousel

--- js/main.js
+++ js/main.js
@@ -4,13 +4,12 @@
 
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
-
 //
 // Place any custom JS here
 //
 
 // Create an example popover
-document.querySelectorAll('[data-bs-toggle="popover"]')
-  .forEach(popover => {
-    new bootstrap.Popover(popover)
-  })
+// document.querySelectorAll('[data-bs-toggle="popover"]')
+//   .forEach(popover => {
+//     new bootstrap.Popover(popover)
+//   })
